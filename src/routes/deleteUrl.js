5ceb0eb45e1d0d5927af42825229ba84957841fd
Gurushesh Pawar <62001513+gurupawar@users.noreply.github.com>
2024-02-21 11:05:04 +0530

    security issue fixed 🐛

--- src/routes/deleteUrl.js
+++ src/routes/deleteUrl.js
@@ -8,30 +8,42 @@ const router = express.Router();
 
 router.delete("/:id", authenticateToken, async (req, res) => {
   const { id } = req.params;
-  const token = req.headers["authorization"];
-  const decode = jwt.verify(token, secret_jwt);
-  console.log(decode._id);
-  console.log(id);
 
   try {
-    const urls = await Url.find({ user: decode._id });
+    const decodedToken = jwt.verify(req.headers["authorization"], secret_jwt);
+    const userId = decodedToken._id;
 
-    console.log(urls);
+    const url = await Url.findOne({ _id: id, user: userId });
+
+    if (!url) {
+      return res.status(404).json({
+        message: "URL not found or you are not authorized to delete it.",
+        status: 404,
+      });
+    }
+
+    // If URL found and belongs to user, delete it
     const deletedUrl = await Url.findByIdAndDelete(id);
 
     if (!deletedUrl) {
-      return res.status(400).json({ message: "Url not found!", status: 400 });
-    } else {
-      res.status(200).json({
-        message: "URL has been successfully deleted 🥲",
-        status: 200,
-      });
+      return res.status(400).json({ message: "URL not found!", status: 400 });
     }
+
+    // Respond with success message
+    res.status(200).json({
+      message: "URL has been successfully deleted.",
+      status: 200,
+    });
   } catch (error) {
+    // Handle errors
     console.error("Error occurred while deleting URL:", error);
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ error: "Unauthorized", status: 401 });
+    }
     return res
       .status(500)
       .json({ error: "Internal Server Error", status: 500 });
   }
 });
+
 module.exports = router;
